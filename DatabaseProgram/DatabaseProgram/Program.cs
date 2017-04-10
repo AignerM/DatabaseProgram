052afc19816412c5fce1351fdefc4db865535960
AignerM <michael.aigner@cellent.de>
2017-04-10 08:49:40 +0200

    first commit

---
+++ DatabaseProgram/DatabaseProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
